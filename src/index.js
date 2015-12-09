0db04fc73c413db116659b59132a4914082abf5e
廖清松 <tenfour@liaoqingsongdeMacBook-Pro.local>
2015-12-09 13:53:15 +0800

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ var FamousEngine = require('famous/core/FamousEngine');
 var DOMElement   = require('famous/dom-renderables/DOMElement');
 
 var Header       = require('Header');
-
+var Footer       = require('Footer');
+var HomeCenter   = require('HomeCenter');
 
 FamousEngine.init();
 
@@ -11,3 +12,12 @@ var scene = FamousEngine.createScene();
 // Header
 var headerNode = new Header();
 scene.addChild(headerNode);
+
+
+// Center
+var homeCenterNode = new HomeCenter();
+scene.addChild(homeCenterNode);
+
+// Footer
+var footerNode = new Footer();
+scene.addChild(footerNode);
